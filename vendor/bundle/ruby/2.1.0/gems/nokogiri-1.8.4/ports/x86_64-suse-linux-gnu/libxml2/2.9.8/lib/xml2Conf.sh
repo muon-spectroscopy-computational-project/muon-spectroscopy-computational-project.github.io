3ed689dad982e218523221088a8cb05523413a49
Simone <wml08325@scpc044.esc.rl.ac.uk>
2018-08-13 11:28:04 +0100

    Added vendor dependencies for Jekyll

---
+++ vendor/bundle/ruby/2.1.0/gems/nokogiri-1.8.4/ports/x86_64-suse-linux-gnu/libxml2/2.9.8/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/wml08325/Desktop/MSCP/web/vendor/bundle/ruby/2.1.0/gems/nokogiri-1.8.4/ports/x86_64-suse-linux-gnu/libxml2/2.9.8/lib"
+XML2_LIBS="-lxml2 -L/lib64 -lz      -lm "
+XML2_INCLUDEDIR="-I/home/wml08325/Desktop/MSCP/web/vendor/bundle/ruby/2.1.0/gems/nokogiri-1.8.4/ports/x86_64-suse-linux-gnu/libxml2/2.9.8/include/libxml2"
+MODULE_VERSION="xml2-2.9.8"
+
---
+++ vendor/bundle/ruby/2.1.0/gems/nokogiri-1.8.4/ports/x86_64-suse-linux-gnu/libxslt/1.1.32/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/wml08325/Desktop/MSCP/web/vendor/bundle/ruby/2.1.0/gems/nokogiri-1.8.4/ports/x86_64-suse-linux-gnu/libxslt/1.1.32/lib"
+XSLT_LIBS="-lxslt -L/home/wml08325/Desktop/MSCP/web/vendor/bundle/ruby/2.1.0/gems/nokogiri-1.8.4/ports/x86_64-suse-linux-gnu/libxml2/2.9.8/lib -lxml2 -L/lib64 -lz -lm -ldl -lm "
+XSLT_INCLUDEDIR="-I/home/wml08325/Desktop/MSCP/web/vendor/bundle/ruby/2.1.0/gems/nokogiri-1.8.4/ports/x86_64-suse-linux-gnu/libxslt/1.1.32/include"
+MODULE_VERSION="xslt-1.1.32"
